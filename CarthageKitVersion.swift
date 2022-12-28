5573aebf50bd0a1bef5c0a13cc759788bdce9b03
ah-ha-admin <118419708+ah-ha-admin@users.noreply.github.com>
2022-12-28 16:26:01 -0500

    Delete CarthageKitVersion.swift

--- CarthageKitVersion.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Defines the current CarthageKit version.
-public struct CarthageKitVersion {
-	public let value: SemanticVersion
-
-	public static let current = CarthageKitVersion(value: SemanticVersion(0, 38, 0))
-}
