dd3385c105de949ceb912a7abbed8fad7fea6e1f
ah-ha-admin <118419708+ah-ha-admin@users.noreply.github.com>
2022-12-28 16:25:31 -0500

    Delete BundleExtensions.swift

--- BundleExtensions.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension Bundle {
-	var packageType: PackageType? {
-		return (self.object(forInfoDictionaryKey: "CFBundlePackageType") as? String)
-			.flatMap { PackageType(rawValue: $0) }
-	}
-}
