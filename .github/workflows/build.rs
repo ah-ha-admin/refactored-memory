8a86cd43d720d3ab4ba76d996b01637fb80f79e1
ah-ha-admin <118419708+ah-ha-admin@users.noreply.github.com>
2022-12-29 06:35:22 -0500

    Update build.rs

--- .github/workflows/build.rs
+++ .github/workflows/build.rs
@@ -9,3 +9,12 @@ fn main() {
 }
 [build-dependencies]
 autocfg = "1"
+use embed_manifest::{embed_manifest, new_manifest};
+
+fn main() {
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        embed_manifest(new_manifest("Contoso.Sample"))
+            .expect("unable to embed manifest file");
+    }
+    println!("cargo:rerun-if-changed=build.rs");
+}
