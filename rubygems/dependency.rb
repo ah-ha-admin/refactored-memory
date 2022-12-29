afd460ff0ef51f73f6f934e851c38c8c8890bda3
ah-ha-admin <118419708+ah-ha-admin@users.noreply.github.com>
2022-12-28 22:17:44 -0500

    Update dependency.rb

--- rubygems/dependency.rb
+++ rubygems/dependency.rb
@@ -36,3 +36,33 @@ def matches_spec? spec
 
   requirement.satisfied_by?(spec.version)
 end
+# File lib/rubygems/dependency.rb, line 119
+def requirement
+  return @requirement if defined?(@requirement) and @requirement
+
+  # @version_requirements and @version_requirement are legacy ivar
+  # names, and supported here because older gems need to keep
+  # working and Dependency doesn't implement marshal_dump and
+  # marshal_load. In a happier world, this would be an
+  # attr_accessor. The horrifying instance_variable_get you see
+  # below is also the legacy of some old restructurings.
+  #
+  # Note also that because of backwards compatibility (loading new
+  # gems in an old RubyGems installation), we can't add explicit
+  # marshaling to this class until we want to make a big
+  # break. Maybe 2.0.
+  #
+  # Children, define explicit marshal and unmarshal behavior for
+  # public classes. Marshal formats are part of your public API.
+
+  # REFACTOR: See above
+
+  if defined?(@version_requirement) && @version_requirement
+    version = @version_requirement.instance_variable_get :@version
+    @version_requirement  = nil
+    @version_requirements = Gem::Requirement.new version
+  end
+
+  @requirement = @version_requirements if defined?(@version_requirements)
+end
+
