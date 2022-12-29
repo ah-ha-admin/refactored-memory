2f3c6af1aedc125bf8536bd04abaeb671e3ffed6
ah-ha-admin <118419708+ah-ha-admin@users.noreply.github.com>
2022-12-28 22:01:13 -0500

    Update build_extensions.rb

--- rubygems/build_extensions.rb
+++ rubygems/build_extensions.rb
@@ -1,3 +1,8 @@
+File lib/rubygems/ext/builder.rb, line 24
+def self.class_name
+  name =~ /Ext::(.*)Builder/
+  $1.downcase
+end
 # File lib/rubygems/ext/builder.rb, line 24
 def self.class_name
   name =~ /Ext::(.*)Builder/
