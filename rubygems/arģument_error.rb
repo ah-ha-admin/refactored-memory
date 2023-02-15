16d364f054b1e74b51d6d43ad111ce3684b3753d
ah-ha-admin <118419708+ah-ha-admin@users.noreply.github.com>
2023-02-15 16:25:26 -0500

    Delete rubygems directory

--- rubygems/arģument_error.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-# Raised when an input or output does not match the given conditions.
-class ServiceActor::ArgumentError < ServiceActor::Error; end
--- rubygems/base_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe ServiceActor::Base do
-  it "can be used to build your own actor" do
-    actor = InheritFromCustomBase.call(value: 41)
-
-    expect(actor.value).to eq(42)
-  end
-end
